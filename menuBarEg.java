5e620e3281690a2c9c297d18fb2c2a2429818629
GautamKhadka1 <163504407+GautamKhadka1@users.noreply.github.com>
2024-11-28 08:43:49 +0545

    Edit MenuBar

--- menuBarEg.java
+++ menuBarEg.java
@@ -6,13 +6,18 @@ public class menuBarEg {
         
         JFrame j = new JFrame();
         j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        j.setSize(300, 300);
+        j.setSize(500, 500);
+
+        ImageIcon ic = new ImageIcon("C:\\Users\\LENOVO\\Desktop\\Java-7sem\\download.png");
 
         JMenuBar mb = new JMenuBar();
+        JMenu logo = new JMenu();
+        logo.setIcon(ic);
+
         JMenu m1 = new JMenu("Home");
         JMenu m2 = new JMenu("Packages");
         JMenu i1 = new JMenu("Pokhara");
-        JMenuItem i2 = new JMenuItem("Chitwan");
+        JMenuItem i2 = new JMenuItem("Chitwan", ic);
         JRadioButtonMenuItem i3 = new JRadioButtonMenuItem("Trekking");
         JRadioButtonMenuItem i4 = new JRadioButtonMenuItem("Hiking");
         ButtonGroup bg = new ButtonGroup();
@@ -22,8 +27,11 @@ public class menuBarEg {
         i1.add(i4);
         m2.add(i1);
         m2.add(i2);
+        mb.add(logo);
+
         mb.add(m1);
         mb.add(m2);
+
         j.setJMenuBar(mb);
         j.setVisible(true);
     }
